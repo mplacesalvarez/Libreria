4c3ab7845ad29dc89f84c46c4e4865bf345b4a50
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-02-19 09:08:59 +0100

    Initial commit

---
+++ src/Libreria.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Libreria {
+
+    public void mensajeporconsola(){
+
+        System.out.println("Esto es un mensaje por consola");
+    }
+
+    public void mensajeporventana(){
+
+        JOptionPane.showInputDialog("Esto es un mensaje por ventana");
+    }
+
+
+
+
+
+
+}
